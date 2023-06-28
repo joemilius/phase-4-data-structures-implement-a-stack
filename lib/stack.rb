81d36415d213c225c28e50b22738dbaa2a78ef3b
Joe Milius <joemilius@gmail.com>
2023-06-27 21:38:13 -0600

    stack methods completed

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,48 @@
 # your code here
+class Stack
+
+    def initialize(limit = '')
+        @stack = []
+
+        @limit = limit
+    end
+
+    def push(value)
+        if full?
+            raise 'Stack Overflow'
+        else
+            @stack << value
+        end
+    end
+
+    def pop
+        @stack.pop
+    end
+
+    def peek
+        @stack[-1]
+    end
+
+    def size
+        @stack.size
+    end
+
+    def empty?
+        @stack.size == 0
+    end
+
+    def full?
+       @limit && @stack.size == @limit
+    end
+
+    def search(value)
+        index = 0
+        while index < @stack.size do
+            if @stack[index] == value
+                return (@stack.size - 1) - index 
+            end
+            index += 1
+        end
+        -1
+    end
+end
